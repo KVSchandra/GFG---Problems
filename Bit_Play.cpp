5a19f39ee2be43cbb8b80b1438c0ab97b0974538
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-10-04 03:00:01 +0530

    Update Bit_Play.cpp

--- Bit_Play.cpp
+++ Bit_Play.cpp
@@ -23,6 +23,6 @@ using namespace std;
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-        cout<<"I am A PIRO coder";
+        cout<<"Shaurya is gay";
     return 0;
 }
