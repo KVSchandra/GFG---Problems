9495e13c3fd811a75bdfb090abc39f0b1741e447
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-10-04 02:59:28 +0530

    Add files via upload

---
+++ Bit_Play.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tc() int t;cin>>t;while(t--)
+#define read(type) readInt<type>()
+#define ll long long
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+        cout<<"I am A PIRO coder";
+    return 0;
+}
